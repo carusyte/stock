f7d7095fe081d2c84e126ba2f8c3e222fe5e3b5a
Jonathan <carusyte@163.com>
2018-07-12 22:54:13 +0800

    missed 2 files

--- db/dbmap.go
+++ db/dbmap.go
@@ -2,11 +2,11 @@ package db
 
 import (
 	"database/sql"
+	"log"
 	"time"
 
 	"github.com/DejaMi/mymysql-pool"
-	"github.com/carusyte/stock/model"
-	"github.com/carusyte/stock/util"
+
 	//mysql driver
 	_ "github.com/go-sql-driver/mysql"
 	"gopkg.in/gorp.v2"
@@ -22,7 +22,9 @@ func Get(create, truncate bool) *gorp.DbMap {
 	// use whatever database/sql driver you wish
 	// db, err := sql.Open("mysql", "tcp:localhost:3306*secu/mysql/123456")
 	db, err := sql.Open("mysql", "mysql:123456@/secu")
-	util.CheckErr(err, "sql.Open failed,")
+	if err != nil {
+		log.Panic("sql.Open failed", err)
+	}
 
 	db.SetMaxOpenConns(64)
 	db.SetMaxIdleConns(64)
@@ -31,29 +33,37 @@ func Get(create, truncate bool) *gorp.DbMap {
 	// construct a gorp DbMap
 	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "utf8"}}
 
-	dbmap.AddTableWithName(model.KlineW{}, "kline_w").SetKeys(false, "Code", "Date", "Klid")
-	dbmap.AddTableWithName(model.KlineM{}, "kline_m").SetKeys(false, "Code", "Date", "Klid")
-	dbmap.AddTableWithName(model.Indicator{}, "indicator_d").SetKeys(false, "Code", "Date", "Klid")
-	dbmap.AddTableWithName(model.IndicatorW{}, "indicator_w").SetKeys(false, "Code", "Date", "Klid")
-	dbmap.AddTableWithName(model.IndicatorM{}, "indicator_m").SetKeys(false, "Code", "Date", "Klid")
-	dbmap.AddTableWithName(model.IndcFeatRaw{}, "indc_feat_raw").SetKeys(false, "Code", "Indc", "Fid")
-	dbmap.AddTableWithName(model.GraderStats{}, "grader_stats").SetKeys(false, "Grader", "Frame", "Score")
+	// dbmap.AddTableWithName(model.KlineW{}, "kline_w").SetKeys(false, "Code", "Date", "Klid")
+	// dbmap.AddTableWithName(model.KlineM{}, "kline_m").SetKeys(false, "Code", "Date", "Klid")
+	// dbmap.AddTableWithName(model.Indicator{}, "indicator_d").SetKeys(false, "Code", "Date", "Klid")
+	// dbmap.AddTableWithName(model.IndicatorW{}, "indicator_w").SetKeys(false, "Code", "Date", "Klid")
+	// dbmap.AddTableWithName(model.IndicatorM{}, "indicator_m").SetKeys(false, "Code", "Date", "Klid")
+	// dbmap.AddTableWithName(model.IndcFeatRaw{}, "indc_feat_raw").SetKeys(false, "Code", "Indc", "Fid")
+	// dbmap.AddTableWithName(model.GraderStats{}, "grader_stats").SetKeys(false, "Grader", "Frame", "Score")
 	if create {
 		err = dbmap.CreateTablesIfNotExists()
-		util.CheckErr(err, "Create tables failed,")
+		if err != nil {
+			log.Panic("Create tables failed", err)
+		}
 	}
 	if truncate {
 		err = dbmap.TruncateTables()
-		util.CheckErr(err, "Truncate tables failed,")
+		if err != nil {
+			log.Panic("Truncate tables failed", err)
+		}
 	}
 
-	util.CheckErr(db.Ping(), "Failed to ping db,")
+	if err != nil {
+		log.Panic("Failed to ping db", err)
+	}
 
 	return dbmap
 }
 
 func GetMySql() (c *pool.Conn) {
 	c, e := p.Get()
-	util.CheckErrNop(e, "failed to get connection from pool")
+	if e != nil {
+		log.Println("failed to get connection from pool", e)
+	}
 	return
 }
